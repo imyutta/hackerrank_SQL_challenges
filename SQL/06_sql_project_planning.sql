dfa9347931b480001b01d5c4fc81ec8a4c018a0f
imyutta <yutta.dyck@gmail.com>
2024-04-10 13:12:37 -0700

    update  06 problem set

--- SQL/06_sql_project_planning.sql
+++ SQL/06_sql_project_planning.sql
@@ -1,5 +1,18 @@
-SELECT 
-    COUNT
+SELECT start_date, new_end_date 
+FROM (
+    SELECT s.start_date, MIN(e.end_date) AS new_end_date 
+    FROM (
+        /*Choose the start dates that are not end dates of other projects*/
+        SELECT start_date FROM projects WHERE start_date NOT IN (SELECT end_date FROM projects)
+    ) AS s, (
+        /*Choose the end dates that are not start dates of other projects*/
+        SELECT end_date FROM projects WHERE end_date NOT IN (SELECT start_date FROM projects)
+    ) AS e
+    WHERE start_date <end_date
+    GROUP BY start_date
+) AS derived_table
 ORDER BY
-    number_of_days ASC,
+    /*  Order by the number of days it took to complete the project in ascending order*/
+    datediff(new_end_date, start_date),
+    /* Then order  by the start date of the project*/
     start_date ASC;
